c18f8739f99bd84bf5d5d9b0b0fdf055995a26cd
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-01-13 12:50:38 -0700

    Try

--- includes/HookHandlers/Installer.php
+++ includes/HookHandlers/Installer.php
@@ -15,7 +15,7 @@ class Installer implements LoadExtensionSchemaUpdatesHook {
 	 * @codeCoverageIgnore Tested by updating or installing MediaWiki.
 	 */
 	public function onLoadExtensionSchemaUpdates( $updater ) {
-		$updater->runMaintenance( CreateView::class );
+		$updater->addExtensionUpdate( [ 'runMaintenance', CreateView::class ] );
 		$updater->addPostDatabaseUpdateMaintenance( DeleteTemplate::class );
 	}
 }
