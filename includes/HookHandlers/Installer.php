6fb8c1d6f2fe9929f35a22e27374b6219164764f
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-01-13 16:23:39 -0700

    Fix

--- includes/HookHandlers/Installer.php
+++ includes/HookHandlers/Installer.php
@@ -4,8 +4,9 @@ declare( strict_types = 1 );
 
 namespace MediaWiki\Extension\DynamicPageList4\HookHandlers;
 
-use MediaWiki\Extension\DynamicPageList4\Maintenance\DropView;
+use MediaWiki\Extension\DynamicPageList4\Maintenance\CreateView;
 use MediaWiki\Extension\DynamicPageList4\Maintenance\DeleteTemplate;
+use MediaWiki\Extension\DynamicPageList4\Maintenance\DropView;
 use MediaWiki\Installer\Hook\LoadExtensionSchemaUpdatesHook;
 
 class Installer implements LoadExtensionSchemaUpdatesHook {
