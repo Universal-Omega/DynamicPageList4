f74cacb8a66be1f860030742e39c342ae2585eb1
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-01-13 14:20:55 -0700

    Fix

--- tests/phpunit/DPLQueryIntegrationTest.php
+++ tests/phpunit/DPLQueryIntegrationTest.php
@@ -28,7 +28,7 @@ class DPLQueryIntegrationTest extends DPLIntegrationTestCase {
 			[ 'DPLTestArticleNoCategory' ],
 			$this->getDPLQueryResults( [
 				'category' => '_none_',
-				'distinct' => false,
+				'distinct' => 'no',
 			], '%PAGE%' ),
 			true
 		);
